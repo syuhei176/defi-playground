7899365b65345265674faab179c60134c7687d4f
syuhei176 <syuhei176@gmail.com>
2025-11-15 17:09:58 +0900

    update solidity version

--- src/SimpleLoggingHook.sol
+++ src/SimpleLoggingHook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
 import {IHooks} from "uniswap-v4-core/interfaces/IHooks.sol";
 import {PoolKey} from "uniswap-v4-core/types/PoolKey.sol";
